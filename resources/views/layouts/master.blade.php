5f8ef5a17b2d29bc879982cbf9df90a6e44af294
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-14 01:28:30 +0800

    Initialized Commit : Beginning of Landing Page

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Buenaroa</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  {{-- VITE --}}
+  @vite(['public/assets'])
+
+  <!-- Favicons -->
+  <link href="assets/img/logobuena.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Vlava
+  * Updated: Mar 10 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/vlava-free-bootstrap-one-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1><a href="index.html"><img src="assets/img/logobuena.png"></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Gallery</a></li>
+          <li class="dropdown"><a href="#"><span>Inclusions</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              
+              <li class="dropdown"><a href="#"><span>Event Styling</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Balloon Arrangement</a></li>
+                  <li><a href="#">Flower Arrangement</a></li>
+                  <li><a href="#">Back Drop</a></li>
+                  <li><a href="#">Machines</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>Catering Services</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Appetizers</a></li>
+                  <li><a href="#">Lunch</a></li>
+                  <li><a href="#">Snacks</a></li>
+                  <li><a href="#">Drinks</a></li>
+                  <li><a href="#">Dessert</a></li>
+                </ul>
+              </li>
+              
+              <li><a href="#">Sound System and Lights Rental</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <h1>BUENAROA'S EVENT AND CATERING SERVICES</h1>
+      <h2>Turning DREAMS into REALITY</h2>
+      <a href="#about" class="btn-get-started scrollto">Know More</a>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row content">
+          <div class="col-lg-6">
+            <h2>About Us</h2>
+            <h3>We mainly focuses in providing Event and Catering Services.</h3>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <p>
+              We specialize in reating a truly captivating and unforgettable experiences. They offer services for various occasions, such as 
+              wedding and parties, and work closely with clients to bring their dream event to life. With a keen
+              sense of beauty and elegance, we strive to make each event milestone for its clients.
+              Our ultimate goal is to create breathaking moments that leave a lasting impression on both clients and their guests.
+            </p>
+            
+            <p class="fst-italic">
+              We are driven by passion and dedicated to crafting truly unique and memorable experiences.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Services Section ======= -->
+
+    <!-- End Services Section -->
+
+    <!-- ======= Featured Section ======= -->
+
+   <!-- End Featured Section -->
+
+    <!-- ======= Testimonials Section ======= -->
+
+ 
+    <!-- ======= Portfolio Section ======= -->
+   
+    <!-- End Portfolio Section -->
+
+    <!-- ======= Team Section ======= -->
+   
+    <!-- End Team Section -->
+
+    <!-- ======= Pricing Section ======= -->
+
+   <!-- End Pricing Section -->
+
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    
+    <!-- ======= Contact Section ======= -->
+    <!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
