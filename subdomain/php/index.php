4b067d3dfbdcab6bfff9581cd8e10372a80124e6
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-14 22:32:37 +0800

    Updated Contact Form Name Submit

--- subdomain/php/index.php
+++ subdomain/php/index.php
@@ -246,7 +246,7 @@ if (isset($_POST['submit'])){ //Check if user enter data
                 <div class="error-message"></div>
                 <div class="sent-message">Your message has been sent. Thank you!</div>
               </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
+              <div class="text-center"><button type="submit" name="submit">Send Message</button></div>
             </form>
           </div>
 
