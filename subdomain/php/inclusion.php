6260789a7b2b9440896ce4b6c0d419f5635c36be
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-17 14:42:58 +0800

    Revert

--- subdomain/php/inclusion.php
+++ subdomain/php/inclusion.php
@@ -1,2 +1,2 @@
-<?php $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/subdomain/php/"; include($IPATH."navigation.php"); ?>
-<h1>This is Inlcusion</h1>
+<?php include('navigation.php');?>
+<h1>Inclusion</h1>
