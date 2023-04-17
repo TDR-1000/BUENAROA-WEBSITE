27d53a6f3289565ad9644f88477ae9839f02bf97
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-17 14:34:16 +0800

    Inclusions Edit

--- subdomain/php/inclusion.php
+++ subdomain/php/inclusion.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php 
+<?php 
     include('navigation.php');
-    ?>
-    <h1>THIS IS INCLUSION</h1>
-</body>
-</html>
+?>
+<h1>This is Inlcusion</h1>
