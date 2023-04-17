fbb8e3b0efc518ff876d6c45f3328a25dfbdfd7c
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-18 03:28:01 +0800

    Revision of Link for Verify Token

--- carpool/verify.php
+++ carpool/verify.php
@@ -37,7 +37,7 @@ function sendemail_verify($userFirstName,$userEmail,$verify_token){
      <h3>We are pleased that you have registered with our carpool service! </h3> 
      <h3>In order to begin using our service, kindly confirm your account by clicking the verification link provided.</h3>
      <hr>
-     <a href='http://localhost/carpool/verify-email.php?token=$verify_token'<button type='button' id='veributton' class='btn btn-info rounded-pill'>Verify Now!</button></a>
+     <a href='https://carpool.buenaroa.store/verify-email.php?token=$verify_token'<button type='button' id='veributton' class='btn btn-info rounded-pill'>Verify Now!</button></a>
      <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4' crossorigin='anonymous'></script>
      <hr>
      <h4>Thank you and Let's RIDE!</h4>";
