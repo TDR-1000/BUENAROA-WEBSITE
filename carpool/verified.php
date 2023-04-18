09e5a9e4d83d3a35f6d0e03e1d84439104d0036c
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-18 08:17:24 +0800

    Try Debugging

--- carpool/verified.php
+++ carpool/verified.php
@@ -38,7 +38,7 @@ $verified_query_run = mysqli_query($conn, $verified);
         </tbody>
     <?//php endwhile; ?>
     </table>
-    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4' crossorigin='anonymous'></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
