c383e0f17671b2fe7a5472e482dbf89fe9d2816e
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-18 08:33:38 +0800

    Hello

--- carpool/verified.php
+++ carpool/verified.php
@@ -1,8 +1,3 @@
-<?php
-include('connection.php');
-$verified = "SELECT * FROM users WHERE verify_status = '1'";
-$verified_query_run = mysqli_query($conn, $verified);
-?>
 <h1>Hello</h1>
 
 <!DOCTYPE html>
@@ -26,16 +21,19 @@ $verified_query_run = mysqli_query($conn, $verified);
         </thead>
         <tbody>
             <?php
-            //while ($data = $verified_query_run->fetch_assoc()) :
+            include('connection.php');
+            $verified = "SELECT * FROM users WHERE verify_status = '1'";
+            $verified_query_run = mysqli_query($conn, $verified);
+            while ($data = $verified_query_run->fetch_assoc()) :
             ?>
                 <tr>
-                    <th scope="row"><?php // echo $data['uID']; ?></th>
-                    <td><?php // echo $data['uUsername']; ?></td>
-                    <td><?php //echo $data['uFirstName']; ?></td>
-                    <td><?php //echo $data['uLastName']; ?></td>
+                    <th scope="row"><?php echo $data['uID']; ?></th>
+                    <td><?php echo $data['uUsername']; ?></td>
+                    <td><?php echo $data['uFirstName']; ?></td>
+                    <td><?php echo $data['uLastName']; ?></td>
                 </tr>
         </tbody>
-    <?//php endwhile; ?>
+    <?php endwhile; ?>
     </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
