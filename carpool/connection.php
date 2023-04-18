0706920b6a7d7b6f66df8951efc8e7fcecc01e90
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-18 08:40:59 +0800

    Changed Database

--- carpool/connection.php
+++ carpool/connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "";
-$password = "";
-$database = "carpool";
+$username = "u235219407_caycay";
+$password = "carpoolniEspino31";
+$database = "u235219407_carpool";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
