3c8a621c7461037f675bd47bf9165d03317e463a
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-18 08:14:17 +0800

    Added semicolon

--- carpool/verified.php
+++ carpool/verified.php
@@ -13,9 +13,7 @@ $verified_query_run = mysqli_query($conn, $verified);
     <title>Verified Users</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-
 <body>
-
     <table class="table">
         <thead>
             <tr>
@@ -30,10 +28,10 @@ $verified_query_run = mysqli_query($conn, $verified);
             while($data = $verified_query_run->fetch_assoc()):
             ?>
                 <tr>
-                    <th scope="row"><?php echo $data['uID'] ?></th>
-                    <td><?php echo $data['uUsername'] ?></td>
-                    <td><?php echo $data['uFirstName'] ?></td>
-                    <td><?php echo $data['uLastName'] ?></td>
+                    <th scope="row"><?php echo $data['uID']; ?></th>
+                    <td><?php echo $data['uUsername']; ?></td>
+                    <td><?php echo $data['uFirstName']; ?></td>
+                    <td><?php echo $data['uLastName']; ?></td>
                 </tr>
         </tbody>
     <?php endwhile;?>
