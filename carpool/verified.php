8325b3377a2814a24f16c7c5e08fb61d9fd6e95b
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-18 08:35:36 +0800

    Try Debbug

--- carpool/verified.php
+++ carpool/verified.php
@@ -22,7 +22,7 @@
             include('connection.php');
             $verified = "SELECT * FROM users WHERE verify_status = '1'";
             $verified_query_run = mysqli_query($conn, $verified);
-            while ($data = $verified_query_run->fetch_assoc()) :
+            while($data = $verified_query_run->fetch_assoc()):
             ?>
                 <tr>
                     <th scope="row"><?php echo $data['uID'] ?></th>
