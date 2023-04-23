c937f0cbf65efd5462dea9b1292316e04eacbda1
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-23 15:52:57 +0800

    Changing Inclusion Page

--- subdomain/inclusion.php
+++ subdomain/inclusion.php
@@ -14,36 +14,10 @@
     <!-- ======= Hero Section ======= -->
     <section id="hero">
         <div class="hero-container">
-            <h1>BUENAROA'S EVENT AND CATERING SERVICES</h1>
-            <h2>Turning DREAMS into REALITY</h2>
-            <a href="#about" class="btn-get-started scrollto">Know More</a>
+            <h1>INCLUSIONS</h1>
+            <a href="#about" class="btn-get-started scrollto">Dive Right In</a>
         </div>
     </section><!-- End Hero -->
-        <!-- ======= About Section ======= -->
-        <section id="about" class="about">
-      <div class="container">
-
-        <div class="row content">
-          <div class="col-lg-6">
-            <h2>About Us</h2>
-            <h3>We mainly focuses in providing Event and Catering Services.</h3>
-          </div>
-          <div class="col-lg-6 pt-4 pt-lg-0">
-            <p>
-              We specialize in reating a truly captivating and unforgettable experiences. They offer services for various occasions, such as 
-              wedding and parties, and work closely with clients to bring their dream event to life. With a keen
-              sense of beauty and elegance, we strive to make each event milestone for its clients.
-              Our ultimate goal is to create breathaking moments that leave a lasting impression on both clients and their guests.
-            </p>
-            
-            <p class="fst-italic">
-              We are driven by passion and dedicated to crafting truly unique and memorable experiences.
-            </p>
-          </div>
-        </div>
-
-      </div>
-    </section><!-- End About Section -->
 
     <main id="main">
         <?php
--- subdomain/navigation.php
+++ subdomain/navigation.php
@@ -37,7 +37,7 @@
             </ul>
           </li>
           <li><a class="nav-link scrollto" id="inclusion-nav" href="inclusion.php"><b>Inclusions</b></a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="index.php#contact">Contact</a></li>
         </ul>
       
         <i class="bi bi-list mobile-nav-toggle"></i>
