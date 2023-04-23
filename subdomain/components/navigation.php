73518585dc926edc804a1141e8b75c0da2a3bca2
caywaii <santiagocc@students.nu-baliwag.edu.ph>
2023-04-23 16:27:52 +0800

    Changed Navigational Structure

--- subdomain/components/navigation.php
+++ subdomain/components/navigation.php
@@ -10,7 +10,7 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-        <li><a class="nav-link scrollto active" href="index.php">Home</a></li>
+        <li><a class="nav-link scrollto" href="index.php">Home</a></li>
           <li><a class="nav-link scrollto" href="index.php#about">About</a></li>
           <li class="dropdown"><a href="#"><span>Gallery</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
--- subdomain/index.php
+++ subdomain/index.php
@@ -82,7 +82,7 @@ if (isset($_POST['submit'])){ //Check if user enter data
 
       <nav id="navbar" class="navbar">
         <ul>
-        <li><a class="nav-link scrollto active" href="index.php">Home</a></li>
+        <li><a class="nav-link scrollto" href="index.php">Home</a></li>
           <li><a class="nav-link scrollto" href="index.php#about">About</a></li>
           <li class="dropdown"><a href="#"><span>Gallery</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
